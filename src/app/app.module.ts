6715be012651cf58eb17f0be859cfb7eef13039b
yacine <yacinebenz3@gmail.com>
2017-03-05 11:29:17 +0000

    generated components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { AngularFireModule } from 'angularfire2';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { EmailComponent } from './email/email.component';
+import { SignupComponent } from './signup/signup.component';
+import { MembersComponent } from './members/members.component';
+
+export const firebaseConfig = {
+  apiKey: 'AIzaSyB8tQxJBKpLc_u_p78jEQIbJk-MdbGWlL0',
+  authDomain: 'angular-authentication-s-f6cea.firebaseapp.com',
+  databaseURL: 'https://angular-authentication-s-f6cea.firebaseio.com',
+  storageBucket: 'angular-authentication-s-f6cea.appspot.com',
+  messagingSenderId: '954182342984'
+};
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    EmailComponent,
+    SignupComponent,
+    MembersComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
