c2235689d94e07bf88d1d04472c9037a4d1215b4
yacine <yacinebenz3@gmail.com>
2017-03-05 12:00:51 +0000

    added classes

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -12,7 +12,28 @@ import { moveIn } from '../router.animations';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  error: any;
+    constructor(public af: AngularFire,private router: Router) {
+
+      this.af.auth.subscribe(auth => {
+      if(auth) {
+        this.router.navigateByUrl('/members');
+      }
+    });
+  }
+  
+  loginGoogle() {
+    this.af.auth.login({
+      provider: AuthProviders.Google,
+      method: AuthMethods.Popup,
+    }).then(
+        (success) => {
+        this.router.navigate(['/members']);
+      }).catch(
+        (err) => {
+        this.error = err;
+      })
+  }
 
   ngOnInit() {
   }
